d2148bc9f035aa9f92b344facb45e37c52e7a6ab
sahdev50 <dxdestroyer007@gmail.com>
2022-06-26 22:10:44 +0100

    sending response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http')
 
 const server =  http.createServer((req, res)=>{
-    console.log(req)
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>Node title</title></head>')
+    res.write('<body><h1>Hello from sahdev!</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(3000)
